adc4bfb37a3e4e92c19e07b0b0d216ade9d92536
wangli <wangli@3commas.cn>
2019-06-05 16:43:58 +0800

    complete builder 0.5.0

---
+++ app/src/main/java/wang/leal/acd/SampleActivity.java
@@ -0,0 +1,14 @@
+package wang.leal.acd;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
